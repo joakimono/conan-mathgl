cbef89fdb9ec7b4940b05c0d1c0b3dafebc24550
Joakim Haugen <joakim.haugen@sintef.no>
2023-07-06 10:27:24 +0200

    Ensure working test_package conan v1

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,8 +1,8 @@
 from os import path
-from conan import ConanFile
+from conan import ConanFile, conan_version
 from conan.tools.build import can_run
 from conan.tools.cmake import cmake_layout, CMake, CMakeToolchain
-
+from conan.tools.scm import Version
 
 class MathglTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
@@ -11,17 +11,20 @@ class MathglTestConan(ConanFile):
 
     def requirements(self):
         self.requires(self.tested_reference_str)
-        self.requires("qt/5.15.9")
-        self.requires("opengl/system")
 
     def layout(self):
         cmake_layout(self)
 
     def generate(self):
         tc = CMakeToolchain(self)
-        tc.variables["WITH_QT"] = self.dependencies["mathgl"].options.qt5
-        tc.variables["WITH_OPENGL"] = self.dependencies["mathgl"].options.opengl and \
-            not self.dependencies["mathgl"].options.shared and self.settings.os == "Windows"
+        if Version(conan_version).major < 2:
+            tc.variables["WITH_QT"] = self.options["mathgl"].qt5
+            tc.variables["WITH_OPENGL"] = self.options["mathgl"].opengl and \
+                not self.options["mathgl"].shared and self.settings.os == "Windows"
+        else:
+            tc.variables["WITH_QT"] = self.dependencies["mathgl"].options.qt5
+            tc.variables["WITH_OPENGL"] = self.dependencies["mathgl"].options.opengl and \
+                not self.dependencies["mathgl"].options.shared and self.settings.os == "Windows"
         tc.generate()
 
     def build(self):
@@ -34,7 +37,8 @@ class MathglTestConan(ConanFile):
             bin_path = path.join(self.cpp.build.bindirs[0], "example")
             self.run(bin_path, env="conanrun")
 
-            if self.dependencies["mathgl"].options.qt5:
+            if (Version(conan_version).major < 2 and self.options["mathgl"].qt5) or \
+               (Version(conan_version).major > 1 and self.dependencies["mathgl"].options.qt5):
                 bin_path = path.join(self.cpp.build.bindirs[0], "qt_example")
 
                 self.run(bin_path + " -platform offscreen", env="conanrun")
